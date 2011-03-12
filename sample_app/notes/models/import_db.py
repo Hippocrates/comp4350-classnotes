9dc7ad84d3affcd8a936bfc729a39affcda60231
Chris <chj3nk1n@gmail.com>
2011-03-12 16:27:33 -0600

    Fixed an error with my last commit.

--- sample_app/notes/models/import_db.py
+++ sample_app/notes/models/import_db.py
@@ -2,6 +2,8 @@
 # in production set to false and it will only be compiled once
 db_debug_mode = True
 
+Note = local_import('logic/objects/note', db_debug_mode).Note
+Course = local_import('logic/objects/course', db_debug_mode).Course
 
 AccessCourseStub = local_import('db/access/course_stub', db_debug_mode).AccessCourseStub
 AccessNoteStub = local_import('db/access/note_stub', db_debug_mode).AccessNoteStub
