a2ea2ea9a92a99955d4664d5dc4a000916eb5dcd
U-stephen-PC\stephen <s_kiazyk@hotmail.com>
2011-03-19 14:40:33 -0500

    Forgot to include updated unit_tests.py with the previous commit.

--- sample_app/notes/unit_tests.py
+++ sample_app/notes/unit_tests.py
@@ -1,11 +1,12 @@
 import modules.db.tests.suite
+import modules.logic.tests.suite
 import unittest
 
 # build a suite up of all the tests
 suite = unittest.TestSuite()
 
-suite.addTest(db.tests.suite.suite()) # db tests
-
+suite.addTest(modules.db.tests.suite.suite()) # db tests
+suite.addTest(modules.logic.tests.suite.suite())
 
 # run the tests
 runner = unittest.TextTestRunner()
