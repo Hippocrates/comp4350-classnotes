b37d59e19fad37affeb7dce43ad85d6c4c81885a
unknown <Daniel@.(none)>
2011-03-24 12:50:26 -0500

    User object commited

---
+++ sample_app/notes/modules/logic/objects/user.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+class User:
+    """ this is a class which holds information about a user entry """
+    def __init__(self, user_id,email,password_hash,lastName,firstName):
+        """ Constructor for user entry.
+        Params: 
+            user_id - the id of the user
+            email - the e-mail of the user
+			password_hash - the password of the user
+			lastName - the last name of the user
+			firstName - the first name of the user
+        """
+        self.user_id = user_id
+        self.email = email
+		self.password_hash = password_hash
+        self.lastName = lastName
+		self.firstName = firstName
+	
+    def __str__(self):
+        """ to string for user entry """
+        return "User [user_id=%s, email=%s, lastName=%s, firstName=%s]" % \
+                    (self.user_id, self.email, self.lastName, self.firstName)
