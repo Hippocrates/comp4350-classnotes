3b6f5142a1f1755d01fd53450b759921e0f01c61
U-stephen-PC\stephen <s_kiazyk@hotmail.com>
2011-04-03 17:52:52 -0500

    Forgot to add some of the files I added on Friday, my bad.

---
+++ sample_app/notes/modules/logic/search/user_search_params.py
@@ -0,0 +1,6 @@
+
+class UserSearchParams:
+    def __init__(self, targetUserName=None, targetFirstName=None, targetLastName=None):
+        self.targetUserName = targetUserName;
+        self.targetFirstName = targetFirstName;
+        self.targetLastName = targetLastName;
